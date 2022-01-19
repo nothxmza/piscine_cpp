921f60631b051f7d53237162ce2e8111abaefb24
nothxmza <you@example.com>
2022-01-19 16:50:05 +0100

    fixe test

--- CP05/ex00/main.cpp
+++ CP05/ex00/main.cpp
@@ -1,12 +1,13 @@
 #include "Bureaucrat.hpp"
-
+//#include <vector>
 
 int main()
 {
+   // std::vector<double> tab(5, 3.14);
     try
 	{
+       //tab.at(10) = 4.;
         Bureaucrat b("lala", 50);
-       // bureaucrat1.decrementer(5);
        std::cout << b<< std::endl;
         b.incrementer(25);
         std::cout << b.getGrade() << std::endl;
