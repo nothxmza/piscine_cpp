aee11adc274d28d154b528e9ec59ff62a2610aa8
nothxmza <77733204+nothxmza@users.noreply.github.com>
2022-02-14 21:22:36 +0100

    Update Animal.hpp

--- CP04/ex00/Animal.hpp
+++ CP04/ex00/Animal.hpp
@@ -8,10 +8,10 @@ class Animal {
         public:
             Animal(void);
             Animal(Animal const & src);
-             virtual ~Animal(void);
+           virtual ~Animal(void);
 			Animal & operator=(Animal const & rhs);
             std::string getType(void) const;
-            virtual  void makeSound(void) const; //oereir
+            virtual  void makeSound(void) const;
 
         protected:
                 std::string type;
